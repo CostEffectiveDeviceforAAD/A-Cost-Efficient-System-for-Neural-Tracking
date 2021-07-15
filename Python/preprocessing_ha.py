b954fddf2b143968c3aa80f5ce9ebed890136a6c
hajiy <hajiy0218@hanyang.ac.kr>
2021-07-16 01:30:38 +0900

    offline

--- Python/preprocessing_ha.py
+++ Python/preprocessing_ha.py
@@ -2,11 +2,20 @@
 import librosa
 from scipy import signal
 from scipy.signal import butter, lfilter, resample, filtfilt
+from scipy.stats import stats
 #from OpenBCI_lsl import *
 import numpy as np
 from pylab import *
 
-def FIR_filter(data, lowcut, highcut, fs, order):
+def butter_bandpass_filter(data, lowcut, highcut, fs, order):   # IIR
+    nyq = 0.5 * fs
+    low = lowcut/nyq
+    high = highcut/nyq
+    b, a = butter(order, [low, high], btype='band')
+    y = filtfilt(b, a, data)
+    return y
+
+def FIR_filter(data, lowcut, highcut, fs, order):   # FIR
     nyq = 0.5 * fs
     low = lowcut/nyq
     high = highcut/nyq
@@ -24,7 +33,10 @@ def Preproccessing(win,srate, low, high, order):
 
     #### Filtering ####
 
-    win = FIR_filter(win, low, high, srate, order)  # order 조정필요.
+    # FIR
+    #win = FIR_filter(win, low, high, srate, order)  # order 조정필요.
+    # IIR
+    win = butter_bandpass_filter(win, low, high, srate, order)
 
     #### Resampling ####
 
@@ -32,7 +44,8 @@ def Preproccessing(win,srate, low, high, order):
 
     ## Z-scoring
 
-    win = (win - win.mean()) / win.std()
+    #win = (win - win.mean()) / win.std()
+    win = stats.zscore(win)
 
     return win
 
@@ -53,3 +66,4 @@ def mfreqz(b,a=1):
     xlabel(r'Normalized Frequency (x$\pi$rad/sample)')
     title(r'Phase response')
     subplots_adjust(hspace=0.5)
+
